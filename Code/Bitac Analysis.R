fbd89c43f12a44ebaa6cca69b2870f4e7012457f
STS912125 <mymobileacount666@gmail.com>
2025-02-02 21:48:50 +0600

    Uploaded

--- Code/Bitac Analysis.R
+++ Code/Bitac Analysis.R
@@ -1,27 +1,33 @@
+#I had to download the data for each day manually here. In this Code, I clean up the table a little :)
+
 library(readxl)
-Machining_Report30 <- read_excel("F:/Sumaiya/R/Playing Around With BITAC Data/Data/Machining Report 30.xlsx")
-Machining_Report29 <- read_excel("F:/Sumaiya/R/Playing Around With BITAC Data/Data/Machining Report 29.xlsx")
-Machining_Report28 <- read_excel("F:/Sumaiya/R/Playing Around With BITAC Data/Data/Machining Report 28.xlsx")
-Machining_Report27 <- read_excel("F:/Sumaiya/R/Playing Around With BITAC Data/Data/Machining Report 27.xlsx")
-Machining_Report26 <- read_excel("F:/Sumaiya/R/Playing Around With BITAC Data/Data/Machining Report 26.xlsx")
 
-Machining_Report8 <- read_excel("F:/Sumaiya/R/Playing Around With BITAC Data/Data/Machining Report 8.xlsx")
+MachiningReportJanuary = read_excel("./Data/Machining Report 31.xlsx")
+ 
+for (i in 30:1){
+  path = paste("./Data/Machining Report ",i,".xlsx",sep = "")
+  MachiningReport = read_excel(path)
+  MachiningReportJanuary = rbind(MachiningReportJanuary , MachiningReport)
+  
+}
+data = MachiningReportJanuary
 
+#Organizing Everything a Little
 
 library(dplyr)
-Machining_Report_26_30 <- rbind(Machining_Report30,Machining_Report29,Machining_Report28,Machining_Report27,Machining_Report26)
-
-data <- Machining_Report8
-
 library(lubridate)
 
 data$`ProducedAtFormatted`=   ymd_hms(data$`Produced At`)
 
-data = data %>%  mutate(`Hour`= hour(data$`ProducedAtFormatted`))
-
-
-data = data %>% mutate(`Time Difference` = as.numeric(difftime(data$`ProducedAtFormatted`, lead(data$`ProducedAtFormatted`))))
+data = data %>% mutate(`Time Difference (second)` = as.numeric(difftime(data$`ProducedAtFormatted`, lead(data$`ProducedAtFormatted`))))
 data$`Time Difference`[nrow(data)] <- 0
+
+data = data %>%  mutate(`Hour`= hour(data$`ProducedAtFormatted`))
+data = data %>%  mutate(`Day`= day(data$`ProducedAtFormatted`))
+
+#confirm_all_days = data %>% group_by(`Day`) %>% summarize(n())
+
+write.csv(data, file= "./Data/MachiningReportJanuary.xlsx")
 
 TimedataByProduct = data %>%  group_by(`Program Name`,`Hour`,`Power Status`) %>%  summarise(sum(`Time Difference`))
 
