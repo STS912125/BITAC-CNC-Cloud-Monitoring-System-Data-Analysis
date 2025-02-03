6ce4de73dcbbff7d59dca6741043a12a61d3acd9
STS912125 <mymobileacount666@gmail.com>
2025-02-03 08:23:30 +0600

    Modified

--- Code/2. Playing Around With the Table.R
+++ Code/2. Playing Around With the Table.R
@@ -7,6 +7,7 @@ TimedataByProduct = data %>% filter(Day == 8 & Station.Name == "HAAS CM-1" & `Ti
 exp = data %>% filter(Day == 8 & Station.Name == "HAAS CM-1" & `Hour` == 9 & `Time.Difference` < 3600) #%>% 
   #group_by(`Hour`) %>%  
   #summarise(sum(`Time.Difference`))
+#basically this is an experiment to confirm the very first time difference is very high
 
 StationNamedata = count(data , `Station.Name` )
 ThisCycleData = count(data , `This.Cycle` )
@@ -17,4 +18,5 @@ DImode = data %>% filter(`Machining.Mode`== "DI") %>%  group_by(`Machining.Mode`
 library(sqldf)
 pl = sqldf("SELECT * from data limit 5")
 jan8data = sqldf("select * from data where Day = 8 and `Station.Name` = 'HAAS CM-1'")
-groupeddata = sqldf("select `Program.Name`, `Hour`, `Power.Status`,sum(`Time.Difference`) as TimeElapsed from jan8data group by `Program.Name`, `Hour`, `Power.Status`")
+groupeddata = sqldf("select `Program.Name`, `Hour`, `Power.Status`,sum(`Time.Difference`) as TimeElapsed from jan8data group by `Program.Name`, `Hour`, `Power.Status` order by hour")
+
