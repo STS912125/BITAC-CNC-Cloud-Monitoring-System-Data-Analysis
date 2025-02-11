2df5c46f789d7c78992ff1764f493eff4feb3e77
STS912125 <mymobileacount666@gmail.com>
2025-02-11 13:15:10 +0600

    Modified

--- Code/2. Playing Around With the Table.R
+++ Code/2. Playing Around With the Table.R
@@ -1,22 +1,24 @@
 data = read.csv("./Data/MachiningReportJanuary.csv")
+library(dplyr)
 
-TimedataByProduct = data %>% filter(Day == 8 & Station.Name == "HAAS CM-1" & `Time.Difference` < 3600) %>% 
-  group_by(`Program.Name`,`Hour`,`Power.Status`) %>%  
-  summarise(sum(`Time.Difference`))
+StationWise_Hourly_Availability = data %>% filter(`Time.Difference` < 7200) %>% 
+  group_by(`Day`,`Station.Name`,`Hour`) %>%  
+  summarise(`Active Time` = sum(`Time.Difference`[`Power.Status` == "ACTIVE"]),
+            `Stopped Time` = sum(`Time.Difference`[`Power.Status` == "STOPPED"])) %>% 
+  arrange(Day, Hour) %>% ungroup() %>% 
+  group_by(`Day`,`Station.Name`) %>% mutate(`Sum of Active Time` = cumsum(`Active Time`)) %>% 
+  mutate(`Elapsed time` = 3600 ) %>% mutate(`Total Elapsed time` = cumsum(`Elapsed time`) ) %>% 
+  mutate(`Availability` = round((`Sum of Active Time`)*100/(`Total Elapsed time`),digits = 2))
 
-exp = data %>% filter(Day == 8 & Station.Name == "HAAS CM-1" & `Hour` == 9 & `Time.Difference` < 3600) #%>% 
-  #group_by(`Hour`) %>%  
-  #summarise(sum(`Time.Difference`))
-#basically this is an experiment to confirm the very first time difference is very high
+StationWise_Daily_Availability = StationWise_Hourly_Availability %>% group_by(`Day`,`Station.Name`) %>% 
+  summarise(`Daily Availability` = last(`Availability`))
 
-StationNamedata = count(data , `Station.Name` )
-ThisCycleData = count(data , `This.Cycle` )
-
-DImode = data %>% filter(`Machining.Mode`== "DI") %>%  group_by(`Machining.Mode`, `Station.Name`) %>%  summarise()
-
+ProductWise_Hourly_Availability = 
+# StationNamedata = count(data , `Station.Name` )
+# ThisCycleData = count(data , `This.Cycle` )
 
 library(sqldf)
-pl = sqldf("SELECT * from data limit 5")
-jan8data = sqldf("select * from data where Day = 8 and `Station.Name` = 'HAAS CM-1'")
-groupeddata = sqldf("select `Program.Name`, `Hour`, `Power.Status`,sum(`Time.Difference`) as TimeElapsed from jan8data group by `Program.Name`, `Hour`, `Power.Status` order by hour")
+# pl = sqldf("SELECT * from data limit 5")
+# jan8data = sqldf("select * from data where Day = 8 and `Station.Name` = 'HAAS CM-1'")
+# groupeddata = sqldf("select `Program.Name`, `Hour`, `Power.Status`,sum(`Time.Difference`) as TimeElapsed from jan8data group by `Program.Name`, `Hour`, `Power.Status` order by hour")
 
